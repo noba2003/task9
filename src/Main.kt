0599a4b8edc422f260b6d725ddabd2c39c312d5d
ahmed <noba200397@gmail.com>
2024-08-24 14:20:48 +0300

    test

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var manager: Employee = Manager("HR", "ahmed", EMP.Manager.toString(), 25)
-    var developer: Employee = Developer(Programming.kotlin.toString(), "ali", EMP.Developer.toString(), 365)
+    val manager: Employee = Manager("HR", "ahmed", EMP.Manager.toString(), 25)
+    val developer: Employee = Developer(Programming.kotlin.toString(), "ali", EMP.Developer.toString(), 365)
     Employee.printDetails(manager)
     Employee.printDetails(developer)
     println("------------")
     manager.work()
-    manager.report()
+    manager.report/**/()
 
     println("------------")
- developer.work()
     developer.work()
+    developer.report()
     println("------------")
     manager.details()
     developer.details()
     println("------------")
     //sealed Class
-    var managerSealed =sealed.Manager("it", "ahmed", EMP.Manager.toString(), 65)
-    var developerSealed =sealed.Developer(Programming.java.toString(), "amer", EMP.Developer.toString(), 45)
+    val managerSealed =sealed.Manager("it", "ahmed", EMP.Manager.toString(), 65)
+    val developerSealed =sealed.Developer(Programming.java.toString(), "amer", EMP.Developer.toString(), 45)
 
     managerSealed.work()
     managerSealed.report()
+    println("------------")
     developerSealed.work()
     developerSealed.report()
 }
